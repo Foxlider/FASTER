d6305216146ddda46088f3851cd2a5f7a75786f6
alec-hs <alec-hs@users.noreply.github.com>
2018-06-28 17:14:02 +0100

    Expanded Navigation
    General layout cleanup. Added the ability for the nav bar to expand in both axis as the window is resized.

--- FAST2/MainWindow.xaml.vb
+++ FAST2/MainWindow.xaml.vb
@@ -1,3 +1,21 @@
 ﻿Class MainWindow
+    Private Sub SteamUpdaterTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles steamUpdaterTabSelect.Selected
+        mainContent.SelectedIndex = 0
+    End Sub
 
+    Private Sub ServerModsTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles serverModsTabSelect.Selected
+        mainContent.SelectedIndex = 1
+    End Sub
+
+    Private Sub SettingsTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles settingsTabSelect.Selected
+        mainContent.SelectedIndex = 2
+    End Sub
+
+    Private Sub ToolsTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles toolsTabSelect.Selected
+        mainContent.SelectedIndex = 3
+    End Sub
+
+    Private Sub AboutTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles aboutTabSelect.Selected
+        mainContent.SelectedIndex = 4
+    End Sub
 End Class
