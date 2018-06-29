f6417130c2114a4624403292abc999caf8275eb0
alec-hs <alec-hs@users.noreply.github.com>
2018-06-29 14:04:02 +0100

    Linked Menu to Tab Control
    Added code to allow menu list items to switch the selected tabs on the main content panel.

--- FAST2/MainWindow.xaml.vb
+++ FAST2/MainWindow.xaml.vb
@@ -1,21 +1,57 @@
-﻿Class MainWindow
-    Private Sub SteamUpdaterTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles steamUpdaterTabSelect.Selected
-        mainContent.SelectedIndex = 0
+﻿Imports System.Text.RegularExpressions
+
+Class MainWindow
+    Private Sub ServerProfLabel_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles serverProfilesLabel.MouseDoubleClick
+        CreateNewServerProfile(InputBox("Enter profile name:", "New Server Profile"))
     End Sub
 
-    Private Sub ServerModsTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles serverModsTabSelect.Selected
-        mainContent.SelectedIndex = 1
+    Public Sub CreateNewServerProfile(profileName As String)
+        Dim safeProfileName As String = SafeName(profileName)
+
+        Dim newServer As New ListBoxItem With {
+            .Content = profileName,
+            .Name = safeProfileName & "Select"
+        }
+
+        serverProfilesList.Items.Add(newServer)
+
+        AddHandler newServer.Selected, AddressOf ServerProfile_Selected
+
+        Dim newTab As New TabItem With {
+            .Name = safeProfileName,
+            .Content = profileName
+            }
+
+        mainContent.Items.Add(newTab)
     End Sub
 
-    Private Sub SettingsTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles settingsTabSelect.Selected
-        mainContent.SelectedIndex = 2
+    Public Sub ServerProfile_Selected(sender As ListBoxItem, e As RoutedEventArgs) Handles steamUpdaterTabSelect.Selected, serverModsTabSelect.Selected, settingsTabSelect.Selected, toolsTabSelect.Selected, aboutTabSelect.Selected
+        Dim menus As New List(Of ListBox) From {
+            mainMenuItems,
+            serverProfilesList,
+            otherMenuItems
+        }
+
+        For Each list In menus
+            For Each item As ListBoxItem In list.Items
+                If item.Name IsNot sender.Name Then
+                    item.IsSelected = False
+                End If
+            Next
+        Next
+
+        For Each item As TabItem In mainContent.Items
+            If item.Name = sender.Name.Replace("Select", "") Then
+                mainContent.SelectedItem = item
+            End If
+        Next
     End Sub
 
-    Private Sub ToolsTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles toolsTabSelect.Selected
-        mainContent.SelectedIndex = 3
-    End Sub
-
-    Private Sub AboutTabSelect_Selected(sender As Object, e As RoutedEventArgs) Handles aboutTabSelect.Selected
-        mainContent.SelectedIndex = 4
-    End Sub
+    Public Function SafeName(ByVal strIn As String) As String
+        Try
+            Return Regex.Replace(strIn, "[^\w\.@-]", "", RegexOptions.None, TimeSpan.FromSeconds(1.5))
+        Catch __unusedRegexMatchTimeoutException1__ As RegexMatchTimeoutException
+            Return String.Empty
+        End Try
+    End Function
 End Class
