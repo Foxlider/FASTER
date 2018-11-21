15e9e380547a79815225d8d544cdb8d84fbd7d28
alec-hs <alec-hs@users.noreply.github.com>
2018-11-21 12:24:25 +0000

    Version Change to 2.0.1.0
    Version change for first public release.

--- FAST2/My Project/AssemblyInfo.vb
+++ FAST2/My Project/AssemblyInfo.vb
@@ -55,6 +55,6 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.0.0.0")>
-<Assembly: AssemblyFileVersion("2.0.0.0")>
+<Assembly: AssemblyVersion("2.0.1.0")>
+<Assembly: AssemblyFileVersion("2.0.1.0")>
 <Assembly: NeutralResourcesLanguage("en-GB")>
