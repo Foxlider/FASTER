48ca370b5e4afda3d782063f3dc9aeeaed0c355f
Keelah <al.loncham@gmail.com>
2020-09-28 23:15:07 +0200

    First stage of Maintenance Program

---
+++ FASTER Maintenance/Models/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FASTER.Models
+{
+    internal static class Functions
+    {
+        // Takes any string and removes illegal characters
+        public static string SafeName(string input, bool ignoreWhiteSpace = false, string replacement = "_")
+        {
+            input = input.Replace("@", "");
+            if (ignoreWhiteSpace)
+            {
+                // input = Regex.Replace(input, "[^a-zA-Z0-9\-_\s]", replacement) >> "-" is allowed
+                input = Regex.Replace(input, @"[^a-zA-Z0-9_\s]", replacement);
+                input = input.Replace(replacement + replacement, replacement);
+                return input;
+            }
+            // input = Regex.Replace(input, "[^a-zA-Z0-9\-_]", replacement) >> "-" is allowed
+            input = Regex.Replace(input, "[^a-zA-Z0-9_]", replacement);
+            input = input.Replace(replacement + replacement, replacement);
+            return input;
+        }
+    }
+}
---
+++ FASTER Maintenance/Models/SteamMod.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace FASTER.Models
+{
+    [Serializable]
+    public class SteamModCollection
+    {
+        [XmlElement(Order = 1)]
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        public string CollectionName { get; set; } = "Main";
+
+        [XmlElement(Order = 2, ElementName = "SteamMod")]
+        public List<SteamMod> SteamMods = new List<SteamMod>();
+    }
+
+    [Serializable]
+    public class SteamMod
+    {
+        private SteamMod()
+        {}
+
+        public SteamMod(uint workshopId, string name, string author, int steamLastUpdated, bool privateMod = false)
+        {
+            WorkshopId = workshopId;
+            Name = name;
+            Author = author;
+            SteamLastUpdated = steamLastUpdated;
+            PrivateMod = privateMod;
+        }
+
+        public uint WorkshopId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public int SteamLastUpdated { get; set; }
+        public int LocalLastUpdated { get; set; }
+        public bool PrivateMod { get; set; }
+        public string Status { get; set; } = "Not Installed";
+        public string Size { get; set; } = "Unknown";
+    }
+}
