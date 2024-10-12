ead88114077ac0a170493595f078d891acd72c3e
Keelah <al.loncham@gmail.com>
2024-10-13 00:59:14 +0200

    Fixed a few warnings, hid others.
    Will fix later
    Fixes #205

--- FASTERTests/Models/EncryptionTests.cs
+++ FASTERTests/Models/EncryptionTests.cs
@@ -1,17 +1,21 @@
 ﻿using NUnit.Framework;
 
+using System.Runtime.Versioning;
+
 namespace FASTER.Models.Tests
 {
     [TestFixture()]
     public class EncryptionTests
     {
+        [SupportedOSPlatform("windows7.0")]
         [Test()]
         public void EncryptDataTest()
         {
             Assert.DoesNotThrow(() => Encryption.Instance.EncryptData("SomeText"));
-            Assert.That(Encryption.Instance.EncryptData("SomeText"), Is.EqualTo("SomeText"));
+            Assert.That(Encryption.Instance.EncryptData("SomeText"), Is.Not.EqualTo("SomeText"));
         }
 
+        [SupportedOSPlatform("windows7.0")]
         [Test()]
         public void DecryptDataTest()
         {
