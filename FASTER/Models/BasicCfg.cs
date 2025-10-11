957c881b7b1adc6f5b956281ff26101e77aacf5e
Yethe Samartaka <55753928+YetheSamartaka@users.noreply.github.com>
2025-10-11 17:13:26 +0200

    Fix for Performance preset for server profiles not copied over

--- FASTER/Models/BasicCfg.cs
+++ FASTER/Models/BasicCfg.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Globalization;
 using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace FASTER.Models
 {
@@ -150,6 +151,7 @@ namespace FASTER.Models
         }
 
         [XmlIgnore]
+        [JsonIgnore]
         public string PerfPreset
         {
             get => "Custom";
